efbef8b4246eb3cd4725ce1022cd841bd39a7999
Guillaume Charmetant <cguille.dev@gmail.com>
2018-12-18 13:57:28 +0100

    use selector from config in extension storage

--- short-focus.js
+++ short-focus.js
@@ -1,9 +1,27 @@
-(function () {
+(async function () {
     'use strict';
+
+    const HOST = window.location.host;
+    const PATHNAME = window.location.pathname;
+
+    const SITE_CONF_KEY = 'config.selector.' + HOST;
+    const PAGE_CONF_KEY = SITE_CONF_KEY + PATHNAME;
+
+    const config = await browser.storage.local.get([SITE_CONF_KEY, PAGE_CONF_KEY]);
+
+    let selector;
+
+    if (config[PAGE_CONF_KEY]) {
+        selector = config[PAGE_CONF_KEY];
+    } else if (config[SITE_CONF_KEY]) {
+        selector = config[SITE_CONF_KEY];
+    } else {
+        return;
+    }
 
     const DO_NOT_UNFOCUS_TAGS = ['input', 'textarea'];
     const SLASH_KEY_CODE = 191;
-    const elementToFocusOn = document.querySelector('#short-focus');// TODO configure selector per web site and/or page
+    const elementToFocusOn = document.querySelector(selector);
 
     if (!elementToFocusOn) {
         return;
