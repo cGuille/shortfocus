d8a7af6549426280ea7761af404de2f4d2c669ee
Guillaume Charmetant <cguille.dev@gmail.com>
2018-12-19 13:44:12 +0100

    save selector of element to be short focused

--- short-focus.js
+++ short-focus.js
@@ -1,6 +1,22 @@
 (async function () {
     'use strict';
 
+    const SLASH_KEY_CODE = 191;
+
+    const FOCUSABLE_ELT_TAGS = ['input', 'textarea'];
+
+    function isFocusable(elt) {
+        if (FOCUSABLE_ELT_TAGS.includes(elt.tagName.toLowerCase())) {
+            return true;
+        }
+
+        if (elt.isContentEditable) {
+            return true;
+        }
+
+        return false;
+    }
+
     const HOST = window.location.host;
     const PATHNAME = window.location.pathname;
 
@@ -9,37 +25,35 @@
 
     const config = await browser.storage.local.get([SITE_CONF_KEY, PAGE_CONF_KEY]);
 
+    const simmer = window.Simmer;
+
     let selector;
 
     if (config[PAGE_CONF_KEY]) {
         selector = config[PAGE_CONF_KEY];
     } else if (config[SITE_CONF_KEY]) {
         selector = config[SITE_CONF_KEY];
-    } else {
-        return;
     }
 
-    const DO_NOT_UNFOCUS_TAGS = ['input', 'textarea'];
-    const SLASH_KEY_CODE = 191;
-    const elementToFocusOn = document.querySelector(selector);
+    window.addEventListener('keyup', async event => {
+        if (event.shiftKey && !event.altKey && event.keyCode === SLASH_KEY_CODE) {
+            if (!document.activeElement || !isFocusable(document.activeElement)) {
+                const elementToFocusOn = document.querySelector(selector);
 
-    if (!elementToFocusOn) {
-        return;
-    }
-
-    window.addEventListener('keyup', event => {
-        if (document.activeElement) {
-            if (DO_NOT_UNFOCUS_TAGS.includes(document.activeElement.tagName.toLowerCase())) {
-                return;
-            }
-
-            if (document.activeElement.isContentEditable) {
-                return;
+                if (elementToFocusOn) {
+                    elementToFocusOn.focus();
+                }
             }
         }
 
-        if (event.shiftKey && event.keyCode === SLASH_KEY_CODE) {
-            elementToFocusOn.focus();
+        if (event.shiftKey && event.altKey && event.keyCode === SLASH_KEY_CODE) {
+            if (document.activeElement && isFocusable(document.activeElement)) {
+                selector = simmer(document.activeElement);
+
+                const keys = {};
+                keys[SITE_CONF_KEY] = keys[PAGE_CONF_KEY] = selector;
+                await browser.storage.local.set(keys);
+            }
         }
     });
 }());
